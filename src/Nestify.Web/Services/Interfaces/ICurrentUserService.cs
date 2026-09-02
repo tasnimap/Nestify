3b96fdf361162d8dae2b66d8286f1e734f679484
Shreon <tasmiashreoshi@gmail.com>
2026-09-02 23:28:40 +0600

    Fixed the design of my bookings and my posts

--- src/Nestify.Web/Services/Interfaces/ICurrentUserService.cs
+++ src/Nestify.Web/Services/Interfaces/ICurrentUserService.cs
@@ -1,5 +1,15 @@
 using Nestify.Web.Services.Interfaces;
 
+namespace Nestify.Web.Services.Interfaces
+{
+    public interface ICurrentUserService
+    {
+        string UserId { get; }
+        string DisplayName { get; }
+        void SwitchTo(string userId, string displayName);
+    }
+}
+
 namespace Nestify.Web.Services.Implementations
 {
     public sealed class MockCurrentUserService : ICurrentUserService
